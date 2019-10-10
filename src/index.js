be7467fe83ff2ac1f6e7da0894d91a7d6762df54
Eugenia Shibkova <shibkova91@gmail.com>
2019-10-10 18:14:08 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let str = expr;
+  let char;
+  let result = '';
+  let step = 10;
+
+  while(str.length >= step) {
+    char = str.slice(0, 10); 
+
+    char.replace(/10|11|\*|0|/g, () => {
+      if (char.match(/10/)) { char = char.replace(/10/g, '.'); }
+      if (char.match(/11/)) { char = char.replace(/11/g, '-'); }
+      if (char.match(/\*/)) { char = char.replace(/\*{10}/, ' '); }
+      if (char.match(/0/)) { char = char.replace(/0/g, '');}  
+    });
+
+    if (MORSE_TABLE[char]) {
+      result += MORSE_TABLE[char];
+    } else {
+      result += ' ';
+    }     
+    
+    str = str.slice(10);   
+  }
+ 
+  return result;    
 }
 
 module.exports = {
